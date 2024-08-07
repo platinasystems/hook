6f19163ee4f00a16fa180ee8d71e994b5c4174b1
Giovanni Cammarata <cammarata.giovanni@gmail.com>
2024-08-07 15:37:24 +0200

    add the kernel extra modules

--- nvidia-headless/check.sh
+++ nvidia-headless/check.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 failed() {
-	printf "NVIDIA Kernel module test suite FAILED\n"
+	printf "NVIDIA/IPMI Kernel module test suite FAILED\n"
 	dmesg | tail -n 40
 	/sbin/poweroff -f
 }
 
-printf "NVIDIA kernel modules test for %s\n" "$(uname -r)"
+printf "NVIDIA/IPMI kernel modules test for %s\n" "$(uname -r)"
 
 # order reflects dependencies
-modules='nvidia nvidia-modeset nvidia-drm nvidia-uvm nvidia-peermem'
+modules='nvidia nvidia-modeset nvidia-drm nvidia-uvm nvidia-peermem ipmi_devintf'
 for module in $modules; do
   modinfo "${module}.ko" || failed
 done
-printf "NVIDIA kernel modules formal check PASSED\n"
+printf "NVIDIA/IPMI kernel modules formal check PASSED\n"
 
-printf "trying insmod nvidia modules...\n"
+printf "trying insmod nvidia/ipmi modules...\n"
 for module in $modules; do
   insmod "${module}.ko"
 done
