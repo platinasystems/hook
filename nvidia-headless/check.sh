3e947b59729b827105a0d161ae25109cf01374ac
Eugenio Siciliano <edsiciliano@auctacognitio.net>
2025-03-31 12:25:31 +0200

    Updated the ubuntu kernel version to 6.8.0-56.58 Added Mellanox OFED kernel modules installation

--- nvidia-headless/check.sh
+++ nvidia-headless/check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 failed() {
-	printf "NVIDIA/IPMI Kernel module test suite FAILED\n"
+	printf "NVIDIA/IPMI/Mellanox Kernel module test suite FAILED\n"
 	dmesg | tail -n 40
 	/sbin/poweroff -f
 }
 
-printf "NVIDIA/IPMI kernel modules test for %s\n" "$(uname -r)"
+printf "NVIDIA/IPMI/Mellanox kernel modules test for %s\n" "$(uname -r)"
 
 # order reflects dependencies
 modules='nvidia nvidia-modeset nvidia-drm nvidia-uvm nvidia-peermem ipmi_devintf'
@@ -15,8 +15,20 @@ for module in $modules; do
 done
 printf "NVIDIA/IPMI kernel modules formal check PASSED\n"
 
+# order reflects dependencies
+mlx_modules='mlx_compat ib_core ib_cm ib_ipoib iw_cm rdma_cm ib_iser ib_isert ib_srp ib_umad ib_uverbs irdma knem mlxfw mlxdevm mlx5_core mlx5-vfio-pci mlx5_ib mlx5_vdpa mst_pci mst_pciconf rdma_ucm scsi_transport_srp'
+for module in mlx_modules; do
+  modinfo "${module}.ko" || failed
+done
+printf "Mellanox kernel modules formal check PASSED\n"
+
 printf "trying insmod nvidia/ipmi modules...\n"
 for module in $modules; do
+  insmod "${module}.ko"
+done
+
+printf "trying insmod mellanox modules...\n"
+for module in mlx_modules; do
   insmod "${module}.ko"
 done
 dmesg | tail -n 40
