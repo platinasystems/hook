503da6b80e6852d1e653795a23aa159a09583ef6
Giovanni Cammarata <cammarata.giovanni@gmail.com>
2025-04-03 19:15:32 +0200

    Fix the check.sh script

--- nvidia-headless/check.sh
+++ nvidia-headless/check.sh
@@ -17,7 +17,7 @@ printf "NVIDIA/IPMI kernel modules formal check PASSED\n"
 
 # order reflects dependencies
 mlx_modules='ib_cm ib_ipoib iw_cm rdma_cm ib_iser ib_isert ib_srp ib_umad irdma knem mlxfw mlxdevm mlx5_core mlx5-vfio-pci mlx5_ib mlx5_vdpa mst_pci mst_pciconf rdma_ucm scsi_transport_srp'
-for module in mlx_modules; do
+for module in $mlx_modules; do
   modinfo "${module}.ko" || failed
 done
 printf "Mellanox kernel modules formal check PASSED\n"
@@ -28,7 +28,7 @@ for module in $modules; do
 done
 
 printf "trying insmod mellanox modules...\n"
-for module in mlx_modules; do
+for module in $mlx_modules; do
   insmod "${module}.ko"
 done
 dmesg | tail -n 40
